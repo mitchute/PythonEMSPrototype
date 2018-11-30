90603799ebcd58f3e9fdec0da0817a3366c08e35
Edwin Lee <leeed2001@gmail.com>
2018-11-30 16:32:49 -0600

    Try Linux build script

---
+++ scripts/build_linux_travis.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+# build the tool
+mkdir build
+cd build
+cmake ..
+make -j 2
+cd ..
+PYTHONPATH=my_py_ems ./build/EnergyPlusPyEMS
+
+# prep for packaging
+mkdir tmp_build
+cp build/EnergyPlusPyEMS tmp_build/
+cp -r my_py_ems tmp_build/
+cp scripts/launch.sh tmp_build/
+
+# create final package
+mkdir release
+tar -zcvf release/PyEMSPrototype_linux_debug.tar.gz -C tmp_build my_py_ems EnergyPlusPyEMS launch.sh
