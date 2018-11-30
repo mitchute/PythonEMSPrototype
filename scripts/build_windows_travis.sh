bb64c89bc280ac22fad2f3fce9d3373596235694
Edwin Lee <leeed2001@gmail.com>
2018-11-30 16:46:58 -0600

    Move mac/win build process to scripts

---
+++ scripts/build_windows_travis.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+# build the tool
+mkdir build
+cd build
+cmake .. -G "Visual Studio 15 Win64"
+cmake --build .
+cd ..
+
+# prep for packaging
+cp "/C/Python37/python37.dll" "build/Debug"
+PYTHONPATH=my_py_ems ./build/Debug/EnergyPlusPyEMS
+mkdir tmp_build
+cp build/Debug/EnergyPlusPyEMS.exe tmp_build/
+cp /C/Python37/python37.dll tmp_build/
+cp -r my_py_ems tmp_build/
+cp scripts/launch.sh tmp_build/
+
+# create the final package
+mkdir release && tar -zcvf release/PyEMSPrototype_Win_Debug.tar.gz -C tmp_build my_py_ems EnergyPlusPyEMS launch.sh python37.dll
