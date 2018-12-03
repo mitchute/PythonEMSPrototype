2e8e9e9450b367eb2b052d5e27cd20f7d8d785ad
Edwin Lee <leeed2001@gmail.com>
2018-12-03 09:32:18 -0600

    Grab embeddable python for Windows

--- scripts/build_windows_travis.sh
+++ scripts/build_windows_travis.sh
@@ -11,10 +11,18 @@ cd ..
 cp /C/Python37/python37.dll build/Release
 PYTHONPATH=my_py_ems ./build/Release/EnergyPlusPyEMS
 
+# grab and extract the embeddable python to get a zip copy of the standard library
+mkdir python_embed
+cd python_embed
+curl https://www.python.org/ftp/python/3.7.1/python-3.7.1-embed-amd64.zip --output python_embed.zip
+/c/Program\ Files/7-Zip/7z.exe x python_embed.zip
+cd ..
+
 # prep for packaging
 mkdir tmp_build
 cp build/Release/EnergyPlusPyEMS.exe tmp_build/
 cp /C/Python37/python37.dll tmp_build/
+cp python_embed/python37.zip tmp_build/
 cp -r my_py_ems tmp_build/
 cp scripts/launch.sh tmp_build/
 
