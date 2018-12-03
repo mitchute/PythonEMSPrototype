28453221d7df27dbc0b930bb392b6d34fea2beb8
Edwin Lee <leeed2001@gmail.com>
2018-12-03 08:52:39 -0600

    Only zip on Windows

--- scripts/build_windows_travis_release.sh
+++ scripts/build_windows_travis_release.sh
@@ -20,5 +20,4 @@ cp scripts/launch.sh tmp_build/
 
 # create the final package
 mkdir release
-/C/Program\ Files/7-zip/7z.exe a release/PyEMSPrototype_Win_Release.zip ./tmp_build/*
-tar -zcvf release/PyEMSPrototype_Win.tar.gz -C tmp_build my_py_ems EnergyPlusPyEMS launch.sh python37.dll
+/C/Program\ Files/7-zip/7z.exe a release/PyEMSPrototype_Win.zip ./tmp_build/*
