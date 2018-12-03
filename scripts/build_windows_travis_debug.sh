23881fc9493f5529928e6700faa4a2dadacb18e2
Edwin Lee <leeed2001@gmail.com>
2018-12-02 20:55:35 -0600

    Should be all green again

--- scripts/build_windows_travis_debug.sh
+++ scripts/build_windows_travis_debug.sh
@@ -18,7 +18,7 @@ cp /C/Python37/python37.dll tmp_build/
 cp -r my_py_ems tmp_build/
 cp scripts/launch.sh tmp_build/
 
-"/C/Program Files/7-zip/7z.exe" -i
+/C/Program\ Files/7-zip/7z.exe --help
 
 # create the final package
 mkdir release && tar -zcvf release/PyEMSPrototype_Win_Debug.tar.gz -C tmp_build my_py_ems EnergyPlusPyEMS launch.sh python37.dll
