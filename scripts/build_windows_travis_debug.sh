67194fa767988245ff9d80ecd3293571682cf734
Edwin Lee <leeed2001@gmail.com>
2018-12-02 20:44:33 -0600

    Try building with g++ on Windows

--- scripts/build_windows_travis_debug.sh
+++ scripts/build_windows_travis_debug.sh
@@ -3,20 +3,22 @@
 # build the tool
 mkdir build
 cd build
-cmake .. -G "Visual Studio 15 Win64"
+cmake -DCMAKE_BUILD_TYPE=Release .. -G "MinGW Makefiles"
 cmake --build .
 cd ..
 
 # test out the built binary
-cp /C/Python37/python37.dll build/Debug
-PYTHONPATH=my_py_ems ./build/Debug/EnergyPlusPyEMS
+cp /C/Python37/python37.dll build/
+PYTHONPATH=my_py_ems ./build/EnergyPlusPyEMS
 
 # prep for packaging
 mkdir tmp_build
-cp build/Debug/EnergyPlusPyEMS.exe tmp_build/
+cp build/EnergyPlusPyEMS.exe tmp_build/
 cp /C/Python37/python37.dll tmp_build/
 cp -r my_py_ems tmp_build/
 cp scripts/launch.sh tmp_build/
+
+"/C/Program Files/7-zip/7z.exe" -i
 
 # create the final package
 mkdir release && tar -zcvf release/PyEMSPrototype_Win_Debug.tar.gz -C tmp_build my_py_ems EnergyPlusPyEMS launch.sh python37.dll
--- scripts/build_windows_travis_release.sh
+++ scripts/build_windows_travis_release.sh
@@ -3,17 +3,17 @@
 # build the tool
 mkdir build
 cd build
-cmake .. -G "Visual Studio 15 Win64"
+cmake .. -G "MinGW Makefiles"
 cmake --build . --config Release
 cd ..
 
 # test out the built binary
-cp /C/Python37/python37.dll build/Debug
-PYTHONPATH=my_py_ems ./build/Release/EnergyPlusPyEMS
+cp /C/Python37/python37.dll build/
+PYTHONPATH=my_py_ems ./build/EnergyPlusPyEMS
 
 # prep for packaging
 mkdir tmp_build
-cp build/Release/EnergyPlusPyEMS.exe tmp_build/
+cp build/EnergyPlusPyEMS.exe tmp_build/
 cp /C/Python37/python37.dll tmp_build/
 cp -r my_py_ems tmp_build/
 cp scripts/launch.sh tmp_build/
