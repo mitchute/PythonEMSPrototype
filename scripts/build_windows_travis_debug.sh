e7f923191157dcb286db12d31b170744379dc417
Edwin Lee <leeed2001@gmail.com>
2018-12-02 20:47:46 -0600

    Go back to VS on Windows

--- scripts/build_windows_travis_debug.sh
+++ scripts/build_windows_travis_debug.sh
@@ -3,17 +3,17 @@
 # build the tool
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release .. -G "MinGW Makefiles"
+cmake .. -G "Visual Studio 15 Win64"
 cmake --build .
 cd ..
 
 # test out the built binary
-cp /C/Python37/python37.dll build/
-PYTHONPATH=my_py_ems ./build/EnergyPlusPyEMS
+cp /C/Python37/python37.dll build/Debug
+PYTHONPATH=my_py_ems ./build/Debug/EnergyPlusPyEMS
 
 # prep for packaging
 mkdir tmp_build
-cp build/EnergyPlusPyEMS.exe tmp_build/
+cp build/Debug/EnergyPlusPyEMS.exe tmp_build/
 cp /C/Python37/python37.dll tmp_build/
 cp -r my_py_ems tmp_build/
 cp scripts/launch.sh tmp_build/
